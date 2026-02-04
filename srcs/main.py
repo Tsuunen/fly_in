eb254e0bedc3adbf3e924a9de09ea993fe1b05f4
Remi Laforgue <relaforg@z3r6p3.42lyon.fr>
2026-02-04 13:17:51 +0100

    Solve but without restricted hub

--- srcs/main.py
+++ srcs/main.py
@@ -2,16 +2,19 @@ from map_parser import MapParser, ParsingError
 from map_display import MapDisplay
 from drone import Drone
 from dijkstra import Dijkstra
+from solver import Solver
+from copy import deepcopy
 
 if (__name__ == "__main__"):
-    parser = MapParser("maps/easy/02_simple_fork.txt")
+    parser = MapParser("maps/challenger/01_the_impossible_dream.txt")
     try:
         map = parser.extract()
-        print(Dijkstra(map).run())
-        # drones = []
-        # for d in range(map.nb_drones):
-        #     drones.append(Drone(str(d + 1), map.start.coord))
-        # display = MapDisplay(map, drones, "output.txt")
-        # display.run()
+        paths = Dijkstra(map).run()
+        drones = []
+        for d in range(map.nb_drones):
+            drones.append(Drone(str(d + 1), map.start.coord))
+        Solver(map, paths, deepcopy(drones)).run()
+        display = MapDisplay(map, drones, "output.txt")
+        display.run()
     except ParsingError as e:
         print(e)
---
+++ srcs/solver.py
@@ -0,0 +1,50 @@
+from map_parser import Map, Hub
+from typing import Dict, List, Tuple
+from drone import Drone
+
+
+class Solver:
+    def __init__(self, map: Map, paths: Dict[str, str],
+                 drones: List[Drone]) -> None:
+        self.map = map
+        self.paths = paths
+        self.drones = drones
+
+    def _get_hub_from_coord(self, coord: Tuple[int, int]) -> Hub | None:
+        for h in self.map.hubs:
+            if (h.coord == coord):
+                return (h)
+        return (None)
+
+    def _get_hub_from_name(self, hub_name: str) -> Hub | None:
+        for h in self.map.hubs:
+            if (h.name == hub_name):
+                return (h)
+        return (None)
+
+    # MANAGE RESTRICTED PISTE considerer la connection comme un hub
+    def run(self):
+        state = {h.name: [] for h in self.map.hubs[::-1]}
+        state[self.map.start.name] = self.drones
+        out = ""
+        while (len(state[self.map.end.name]) < self.map.nb_drones):
+            step_str = ""
+            for (curr_hub, drones) in state.items():
+                if (curr_hub == self.map.end.name):
+                    continue
+                src = self._get_hub_from_name(curr_hub)
+                for d in list(drones):
+                    for p in self.paths[src.name]:
+                        dest = self._get_hub_from_name(p[0])
+                        if (dest.name != self.map.end.name and
+                                dest.max_drones <= len(state[dest.name])):
+                            continue
+                        state[src.name].remove(d)
+                        state[dest.name].append(d)
+                        d.coord = dest.coord
+                        step_str += f" D{d.id}-{dest.name}"
+                        break
+            out += step_str.strip() + "\n"
+        with open("output.txt", "w") as file:
+            file.write(out)
+        print(out)
